d389ef376ee54f6f94cc33996b71a2e74a0a35e5
maybeisrael <156699902+maybeisrael@users.noreply.github.com>
2024-03-14 12:22:09 +0100

    first commit

--- src/app/components/Player.tsx
+++ src/app/components/Player.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container } from 'react-bootstrap/Button';
+import  Container from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';  
 
 export default function Player() {
